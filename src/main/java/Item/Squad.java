d337ad2c0c7d8861868ec69e436f19fbd95a2624
AlphaniaMuthee <walphania2@gmail.com>
2020-04-26 12:18:18 +0300

    Add squad defining characteristics array to Squad.java and create SquadTest.java

--- src/main/java/Item/Squad.java
+++ src/main/java/Item/Squad.java
@@ -1,4 +1,22 @@
 package Item;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    private int id;
+    private String Title;
+    private String Ideal;
+    private int Maximum ;
+    private static ArrayList<Squad> mInstances = new ArrayList<>();
+    private List<Hero> heroes ;
+
+    public Squad (String title, String ideal, int maximum) {
+        this.Title = title;
+        this.Ideal = ideal;
+        this.Maximum = maximum;
+        this.mInstances.add(this);
+        id = mInstances.size();
+        heroes = new ArrayList<Hero>();
+    }
 }
