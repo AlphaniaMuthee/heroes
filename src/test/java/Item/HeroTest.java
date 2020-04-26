81535231aef9cb23aec3d1bad787aa95a95668e8
AlphaniaMuthee <walphania2@gmail.com>
2020-04-26 12:52:50 +0300

    add tests to get hero characteristics

--- src/test/java/Item/HeroTest.java
+++ src/test/java/Item/HeroTest.java
@@ -1,7 +1,33 @@
 package Item;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
+
+    @Test
+    public void Hero_available() {
+        Hero test = new Hero ("Wolverine", 38, "Retractable bone claws & superhuman strength", "Poor eye-sight");
+        assertEquals("Wolverine", test.getName());
+    }
+
+    @Test
+    public void Hero_Age() {
+        Hero test = new Hero ("Wolverine", 38, "Retractable bone claws & superhuman strength", "Poor eye-sight");
+        assertEquals( 38, test.getAge());
+    }
+
+    @Test
+    public void Hero_Proficiency() {
+        Hero test = new Hero ("Wolverine", 38, "Retractable bone claws & superhuman strength", "Poor eye-sight");
+        assertEquals("Retractable bone claws & superhuman strength", test.getProficiency());
+    }
+
+    @Test
+    public void Hero_Fragility() {
+        Hero test = new Hero ("Wolverine", 38, "Retractable bone claws & superhuman strength", "Poor eye-sight");
+        assertEquals("Poor eye-sight", test.getFragility());
+    }
 
 }
